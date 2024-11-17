0fc029f34b71bf19f9369dc8e2259248280aac12
Teranes <ranjithteranes10@gmail.com>
2024-11-18 04:39:52 +0530

    Automated build commit

--- src/web-components.ts
+++ src/web-components.ts
@@ -4,17 +4,16 @@ import './assets/css/web-components-styles.css'
 
 import { defineWebComponent, registerWebComponent } from './shared/web-components'
 import webComponents from './exports.json'
-type WebComponent = keyof typeof webComponents
+
 type RegisterOptions = { prefix?: string }
 
 export { componentColors, type ComponentColor } from './shared/values/colors'
 export { defineWebComponent, registerWebComponent } from './shared/web-components'
 export async function register({ prefix = 't' }: RegisterOptions = {}) {
     const exports = (window as any)?.WebComponents || {}
-    for (const exportName in exports) {
-        const componentName = webComponents[exportName as WebComponent];
-        if (componentName) {
-            const component = exports[exportName];
+    for (const [exportName, componentName] of Object.entries(webComponents)) {
+        const component = exports[exportName];
+        if (component) {
             const customElement = defineWebComponent(component)
             registerWebComponent(componentName, customElement, { prefix });
         }
